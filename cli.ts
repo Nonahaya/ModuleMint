41f5cb311ac6db6a242f2ce4feccaf9329dba212
nonahaya <notef68@live.com>
2024-06-17 14:01:02 +0700

    wip Cli

---
+++ cli.ts
@@ -0,0 +1,11 @@
+import { program } from "commander";
+
+let pro = program;
+pro
+  .command("install <module>, <opction>")
+  .description("Install Module to your node_modules")
+  .aliases(["--i", "-i", "i"])
+  .action((modulename: string,opt:string) => /*something*/{})
+pro.command("remove <module>").aliases(["r","-r","--r"])
+.description("Remove Module")
+pro.parse();
